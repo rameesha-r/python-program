5b63aead2dc8720a0d9b3d68b4eb58061c329733
rameesha-r <82645085+rameesha-r@users.noreply.github.com>
2021-05-09 22:08:01 +0530

    Update javasample.java

--- javasample.java
+++ javasample.java
@@ -1,7 +1,9 @@
-public class HelloWorld {
-
-    // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-	System.out.println ("Hello World!");
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
     }
 }
